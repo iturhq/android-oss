880009c480ef96c23de2d056aa1a86802cb1643c
Max Noé <154372+mnohe@users.noreply.github.com>
2026-09-16 21:50:19 +0200

    style(map): order map content imports
    Signed-off-by: Max Noé <154372+mnohe@users.noreply.github.com>

--- feature/map/src/main/kotlin/cat/itur/app/feature/map/ui/MapScreenContent.kt
+++ feature/map/src/main/kotlin/cat/itur/app/feature/map/ui/MapScreenContent.kt
@@ -22,9 +22,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.boundsInRoot
 import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import cat.itur.app.core.domain.model.User
